2bf83760cff7c8e6b01f422f01d0ea5f1625a3b5
dayounglee <verdantjuly@kakao.com>
2024-11-26 11:02:01 +0900

    [Feat] role admin

--- src/comments/controllers/admin-comment.controller.ts
+++ src/comments/controllers/admin-comment.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { CommentsService } from '../services';
 import { AuthGuard } from '@nestjs/passport';
+import { Roles } from 'src/user/guard/auth.guard';
 
 @Controller('api/comments')
 export class AdminCommentController {
   constructor(private readonly commentService: CommentsService) {}
 
   @Get(':commentId')
-  @UseGuards(AuthGuard('admin'))
+  @UseGuards(AuthGuard())
+  @Roles('admin')
   getComment(@Param() param: { commentId: string }) {
     const { commentId } = param;
     return this.commentService.adminFindComment(commentId);

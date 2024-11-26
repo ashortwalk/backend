d50f1042fd99bcabcdcb6e247dc81eb3bd630deb
dayounglee <verdantjuly@kakao.com>
2024-11-26 11:02:55 +0900

    [Feat] role admin

--- src/user/controllers/report.controller.ts
+++ src/user/controllers/report.controller.ts
@@ -13,13 +13,15 @@ import { CreateReportDto } from '../dto/create-report.dto';
 import { ReportService } from '../services/reports.service';
 import { AuthGuard } from '@nestjs/passport';
 import { TokenPayload } from '../types/user.type';
+import { Roles } from '../guard/auth.guard';
 
 @Controller('api/reports')
 export class ReportController {
   constructor(private readonly reportService: ReportService) {}
 
   @Get()
-  @UseGuards(AuthGuard('admin'))
+  @UseGuards(AuthGuard())
+  @Roles('admin')
   getReports(@Query() query) {
     const { page } = query;
     return this.reportService.findReports(page);
@@ -36,7 +38,8 @@ export class ReportController {
   }
 
   @Delete(':reportId')
-  @UseGuards(AuthGuard('admin'))
+  @UseGuards(AuthGuard())
+  @Roles('admin')
   async deleteReport(@Req() req, @Param() param: { reportId: string }) {
     const { reportId } = param;
     const authorization = req.headers.authorization;

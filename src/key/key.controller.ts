c96e3d47699e10a17c65321d8f84a3c218c37210
choeh124 <delicoffee@daum.net>
2024-11-29 12:00:33 +0900

    [Feat]stt controller

--- src/key/key.controller.ts
+++ src/key/key.controller.ts
@@ -2,10 +2,15 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('api/key')
 export class KeyController {
-  constructor() {}
+  constructor() { }
 
   @Get('gpt')
   async gptKey() {
     return { gptKey: process.env.GPT_API_KEY };
+  }
+
+  @Get('stt')
+  async sttKey() {
+    return { sttKey: process.env.STT_API_KEY };
   }
 }

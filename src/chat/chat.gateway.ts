abf73baca9a4e126c5ca997c97527a9f3ffb85bf
dayounglee <verdantjuly@kakao.com>
2024-12-04 15:19:02 +0900

    [Fix] css

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -19,7 +19,7 @@ import { JwtService } from '@nestjs/jwt';
     origin: [
       'http://localhost:3000',
       'http://127.0.0.1:3000',
-      'http://20.41.86.171/',
+      'http://20.41.86.171',
     ],
     methods: ['GET', 'POST'],
     credentials: true,

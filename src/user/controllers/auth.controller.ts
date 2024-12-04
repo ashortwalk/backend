04b0d7982c6da1cc9eb74488533f8d621b9822b4
dayounglee <verdantjuly@kakao.com>
2024-12-04 19:43:03 +0900

    [Fix] https

--- src/user/controllers/auth.controller.ts
+++ src/user/controllers/auth.controller.ts
@@ -30,7 +30,7 @@ export class AuthController {
   async kakaoCallback(@Req() req, @Res() res) {
     const result = await this.authService.kakaoLogin(req);
     return res.redirect(
-      `https://resmemory.store/posts?refreshtoken=${result.refreshToken}&accesstoken=${result.accessToken}`,
+      `https://devleveling.com/posts?refreshtoken=${result.refreshToken}&accesstoken=${result.accessToken}`,
     );
   }
 

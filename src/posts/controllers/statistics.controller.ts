c8fdc799aee1794b2faa47256cf130c4d3062424
dayounglee <verdantjuly@kakao.com>
2024-12-07 09:40:25 +0900

    [Fix] admin page

--- src/posts/controllers/statistics.controller.ts
+++ src/posts/controllers/statistics.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { PostService } from '../services/posts.service';
 import { AuthGuard } from '@nestjs/passport';
-import { Roles } from 'src/user/guard/auth.guard';
 import { TokenPayload } from 'src/user/types/user.type';
 
 @Controller('api/statistics')
@@ -9,7 +8,6 @@ export class StatisticsController {
   constructor(private readonly postService: PostService) {}
   @Get('category')
   @UseGuards(AuthGuard())
-  @Roles('user')
   async statistics(@Req() req: { user: TokenPayload }) {
     const userId = req.user.id;
     return await this.postService.statisticsByCategory(userId);
